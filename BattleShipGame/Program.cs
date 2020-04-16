711882a454a7de044414925722c227faddc7a352
Dana Kiehl <dana.palm87@gmail.com>
2020-04-16 10:01:15 -0500

    Init

---
+++ BattleShipGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display rules
+            //Create Player 1 board game
+            //Create Player 2 board game
+
+
+        }
+    }
+}
