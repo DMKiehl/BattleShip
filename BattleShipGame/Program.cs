e735cb423561fc2b42751057d859ad8ccec58e5d
Dana Kiehl <dana.palm87@gmail.com>
2020-04-16 13:13:44 -0500

    Added a GamePlay class, ships class, Board class, and Player class. Added member variables to Ships Class, Commented in the outline of the project needs and game play.

--- BattleShipGame/Program.cs
+++ BattleShipGame/Program.cs
@@ -10,9 +10,24 @@ namespace BattleShipGame
     {
         static void Main(string[] args)
         {
+            //Other requirements
+            //board must be hidden from opposing player once ships are placed
+            //board must show location of all hits (but hides parts of ship that are not hit yet) and previously chosen misses to opposing player
+            //must keep track of which ships have been completely destroyed
+            //ships must not overlap when placed
+
+
+            //Game Play
             //Display rules
             //Create Player 1 board game
             //Create Player 2 board game
+            //Player 1 places their ships
+            //Player 2 places their ships
+            //Opposing players console and their own console prints out for reference
+            //Current player chooses location to battle
+            //If hit, mark location as a hit, if miss mark location as miss
+            //If ship is sunk delete ship from list
+            //Game goes on until one player sinks all of opposing players ships
 
 
         }
---
+++ BattleShipGame/Ships.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipGame
+{
+    class Ships
+    {
+        //member variables
+        public string ShipType { get; set; }
+
+        public int SpaceSize { get; set; }
+        //constructors
+
+        //member methods
+
+
+    }
+}
